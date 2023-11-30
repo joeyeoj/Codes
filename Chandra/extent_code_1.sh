053f822686180db262b5f6a1a050c2d78baf8b60
joeyeoj <3241716864@qq.com>
2023-11-30 22:42:41 +0800

    231130

--- Chandra/extent_code_1.sh
+++ Chandra/extent_code_1.sh
@@ -38,7 +38,6 @@ photon_counts_list=()
 radius_counts_list=()
 photon_counts_temp=0
 photon_counts_background=()
-counts=0
 for ((i=0;i<16;i++))
 do
     # 单位分别为hms，dms，pixel, 创建源光谱,extracting the pulse height spectrum
@@ -91,29 +90,28 @@ upper_bound=mean+std_dev
 
 radius_counts = [float(x) for x in '${radius_counts_str}'.split(',')]
 photon_counts = [float(x) for x in '${photon_counts_str}'.split(',')]
-lower_limit = [round(float(x)-np.sqrt(float(x))) for x in '${photon_counts_str}'.split(',')]
-upper_limit = [round(float(x)+np.sqrt(float(x))) for x in '${photon_counts_str}'.split(',')]
-photon_counts_err = [lower_limit,upper_limit]
-plt.figure(figsize=(10, 6))
+photon_counts_err=[round(np.sqrt(float(x))) for x in '${photon_counts_str}'.split(',')]
+plt.figure(figsize=(10, 8))
 plt.errorbar(radius_counts, photon_counts, yerr=photon_counts_err, fmt='o', color='purple')
 plt.xlim(0,16)
 # 画出误差范围
-plt.fill_between([0,16], lower_bound, upper_bound, color='gray', alpha=0.5, label='$\mathrm{1\sigma~Range~is~}$'+str(lower_bound)+'-'+str(upper_bound))
-plt.axhline(mean, color='red', label='$\mathrm{background_{mean}=}$'+str(mean))
-plt.axhline(mean+3*std_dev, color='green', label='$\mathrm{3\sigma=}$'+str(mean+3*std_dev))
+plt.fill_between([0,16], lower_bound, upper_bound, color='gray', alpha=0.5, label='$\mathrm{1\sigma~Range~is~}$'+str(round(lower_bound))+'-'+str(round(upper_bound)))
+plt.axhline(mean, color='red', label='$\mathrm{background_{mean}=}$'+str(round(mean)))
+plt.axhline(mean+3*std_dev, color='green', label='$\mathrm{3\sigma=}$'+str(round(mean+3*std_dev)))
 plt.xlabel('Radius (pixel)')
 plt.ylabel('Photons')
 plt.title('Photons vs Radius')
 plt.legend()
-plt.savefig('photons_vs_radius(pixel).png')
+plt.savefig('photons_vs_radius(pixel).png', dpi=300)
 plt.close()
 
 outliers = [(r, p) for r, p in zip(radius_counts, photon_counts) if p > upper_bound]
-print('the boundary of radius is: '+str(max(outliers, key=lambda x: x[0])))
+radius_max=round(max(outliers, key=lambda x: x[0])[0]+0.5)
+print('the boundary of radius (pixel) is: '+str(radius_max))
 
 # 用arcsec表示radius
 radius_counts_arcsec = [round(float(x)*0.492,3) for x in '${radius_counts_str}'.split(',')]
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(10, 8))
 plt.errorbar(radius_counts_arcsec, photon_counts, yerr=photon_counts_err, fmt='o', color='purple')
 upper_x=16*0.492
 plt.xlim(0,upper_x)
@@ -124,157 +122,170 @@ plt.xlabel('Radius (arcsec)')
 plt.ylabel('Photons')
 plt.title('Photons vs Radius')
 plt.legend()
-plt.savefig('photons_vs_radius(arcsec).png')
+plt.savefig('photons_vs_radius(arcsec).png', dpi=300)
 plt.close()
 
 # 检查哪些值超出了误差范围
 outliers = [(r, p) for r, p in zip(radius_counts_arcsec, photon_counts) if p > upper_bound]
-print('the boundary of radius is: '+str(max(outliers, key=lambda x: x[0])))
+radius_max=round(max(outliers, key=lambda x: x[0])[0]+0.5*0.492,3)
+print('the boundary of radius (arcsec) is: '+str(radius_max))
 "
 python -c "$python_code_photon_counts"
+output=$(python -c "$python_code_photon_counts")
+line=$(echo "$output" | grep "the boundary of radius (pixel) is:")
+radius_max=${line##* }
 
-#     # Compare the photon rate with the previous photon rate
-#     # echo "$rate < 1" | bc -l会输出1如果$rate小于1，否则输出0。
-#     # 然后，[ $(echo "$rate < 1" | bc -l) -eq 1 ]会检查这个输出是否等于1，如果等于1，那么整个if语句就为真。
-#     if [ $(echo "$rate < 1" | bc -l) -eq 1 ]
-#     then
-#         number=$((number+1))
-#         if [ ${number} -eq 1 ]
-#         then
-#             ## 删除最后一个元素
-#             # radius_values=("${radius_values[@]:0:${#radius_values[@]}-1}")
-#             # photon_values=("${photon_values[@]:0:${#photon_values[@]}-1}")
-#             # rate_values=("${rate_values[@]:0:${#rate_values[@]}-1}")
-#             radius_final=${prev_radius}
-#             echo "Extracted the majority of photons within a radius of ${prev_radius}"
-#             echo "photon rate is ${prev_rate}"
-#         fi
-#     fi
+specextract "${pfolder}/${obsid}/repro/acisf0${obsid1}_repro_evt2.fits[sky=circle(${ra},${dec},${radius_max})]" ${name} verbose=0 clob+
+# read -p 'enter the energy lower limit (keV), e.g., 0.4 ' elow
+# read -p 'enter the energy upper limit (keV), e.g., 6.0 ' ehigh
+elow=0.4
+ehigh=6.0
+#只对reduced statistic进行判断
+python_code_groupcounts="
+from sherpa.astro.ui import *
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+import numpy as np
 
-#     # Update the previous photon count
-#     prev_photon_count=${photon_count}
-#     prev_radius=${i}
-#     prev_rate=${rate}
-# done
+data_nh=[]
+data_gamma=[]
+data_ampl=[]
+data_nh_error_min=[]
+data_gamma_error_min=[]
+data_ampl_error_min=[]
+data_nh_error_max=[]
+data_gamma_error_max=[]
+data_ampl_error_max=[]
+data_nh_lower=[]
+data_gamma_lower=[]
+data_ampl_lower=[]
+data_nh_upper=[]
+data_gamma_upper=[]
+data_ampl_upper=[]
+rstat_list=[]
+for count in range(5, 105, 5):
+    clean()
+    load_data(\"${name}.pi\")
+    group_counts(count)
+    notice(${elow},${ehigh})
+    set_source( xsphabs.abs1 * powlaw1d.p1)
+    abs1.nH = 0.5
+    guess(p1)
+    fit()
+    fit_results = get_fit_results()
+    reduced_stat = fit_results.rstat
+    rstat_list.append(round(reduced_stat,6))
+    # set_conf_opt(\"sigma\", 1)
+    conf()
+    conf_results=get_conf_results()
+    best_fit_values=conf_results.parvals
+    errors=(conf_results.parmins, conf_results.parmaxes)
+    data_nh.append(round(best_fit_values[0],6))
+    data_gamma.append(round(best_fit_values[1],6))
+    data_ampl.append(round(best_fit_values[2],6))
+    data_nh_error_min.append(abs(round(errors[0][0],6)))
+    data_gamma_error_min.append(abs(round(errors[0][1],6)))
+    data_ampl_error_min.append(abs(round(errors[0][2],6)))
+    data_nh_error_max.append(abs(round(errors[1][0],6)))
+    data_gamma_error_max.append(abs(round(errors[1][1],6)))
+    data_ampl_error_max.append(abs(round(errors[1][2],6)))
+    data_nh_lower.append(round(best_fit_values[0]+errors[0][0],6))
+    data_gamma_lower.append(round(best_fit_values[1]+errors[0][1],6))
+    data_ampl_lower.append(round(best_fit_values[2]+errors[0][2],6))
+    data_nh_upper.append(round(best_fit_values[0]+errors[1][0],6))
+    data_gamma_upper.append(round(best_fit_values[1]+errors[1][1],6))
+    data_ampl_upper.append(round(best_fit_values[2]+errors[1][2],6))
+    # 对ID为1的数据集, 设置x轴的单位为能量, y轴的单位为计数率, 并且不对数据进行任何缩放。
+    set_analysis(1, \"energy\", \"rate\", factor=1)
+    # 绘制模型的预测光谱，而不是原始的观测数据。
+    # 这个函数会根据当前设置的模型参数，计算模型在每个能量点的预测值，并将这个预测光谱绘制出来。
+    plot_fit_delchi()
+    plt.savefig(\"source_flux_chart_\"+str(count)+\".png\")
+    # # 创建一个表格的数据
+    # table_data = [[\"count\", count, \"\"],
+    #             [\"\", \"Best-fit\", \"Error_min\", \"Error_max],
+    #             [\"abs1.nH\", best_fit_values[0], errors[0][0], errors[1][0]],
+    #             [\"p1.gamma\", best_fit_values[1], errors[0][1], errors[1][1]],
+    #             [\"p1.ampl\", best_fit_values[2], errors[0][2], errors[1][2]]]
+    # # 打开一个txt文件，如果这个文件不存在，那么就创建这个文件
+    # with open(\"table.txt\", \"a\") as file:
+    #     # 遍历表格的每一行
+    #     for row in table_data:
+    #         # 将这一行的数据转换为字符串，并用制表符连接这些字符串
+    #         line = \"\t\".join(str(x) for x in row)
+    #         # 将这一行的数据写入文件，并在最后添加一个换行符
+    #         file.write(line + \"\n\")
+    from sherpa_contrib.chart import *
+    save_chart_spectrum(\"source_flux_chart_\"+str(count)+\".dat\", elow=${elow}, ehigh=${ehigh})
+data=[[count for count in range(5, 105, 5)], rstat_list, data_nh, data_nh_lower, data_nh_upper, data_gamma, data_gamma_lower, data_gamma_upper, data_ampl, data_ampl_lower, data_ampl_upper]
+data_transposed=list(map(list, zip(*data)))
+title=[\"count\", \"Reduced Stat\", \"nH\", \"nH_lower\", \"nH_upper\", \"gamma\", \"gamma_lower\", \"gamma_upper\", \"ampl\", \"ampl_lower\", \"ampl_upper\"]
+fig, ax =plt.subplots(1,1)
+ax.axis('tight')
+ax.axis('off')
+the_table = ax.table(cellText=data_transposed, colLabels=title, cellLoc = 'center', loc='center')
+# 设置表格字体
+font = FontProperties()
+font.set_family('Menlo')
+font.set_name('Menlo')
+font.set_style('normal')
 
-# echo "radius_values: ${radius_values[@]}"
-# echo "photon_values: ${photon_values[@]}"
-# echo "rate_values: ${rate_values[@]}"
+the_table.auto_set_font_size(False)
+the_table.set_fontsize(5)
+the_table.scale(1, 1)
+the_table.auto_set_column_width(col=list(range(len(title))))
 
-# {
-#     printf "%s\n" "radius_values: ${radius_values[@]}"
-#     printf "%s\n" "photon_values: ${photon_values[@]}"
-#     printf "%s\n" "rate_values: ${rate_values[@]}"
-# } > output.txt
+plt.savefig('fit_table.png',dpi=300)
 
-# # Convert the arrays to strings
-# radius_values_str=$(printf ",%s" "${radius_values[@]}")
-# photon_values_str=$(printf ",%s" "${photon_values[@]}")
-# rate_values_str=$(printf ",%s" "${rate_values[@]}")
+plt.figure(figsize=(10, 8))
+plt.errorbar([count for count in range(5, 105, 5)], data_nh, yerr=[data_nh_error_min, data_nh_error_max], fmt='o', color='purple')
+plt.xlabel('Group Counts')
+plt.ylabel('abs1.nH')
+plt.title('abs1.nH vs Group Counts')
+plt.savefig('abs1.nH_vs_Group_Counts.png', dpi=300)
+plt.close()
+plt.figure(figsize=(10, 8))
+plt.errorbar([count for count in range(5, 105, 5)], data_gamma, yerr=[data_gamma_error_min, data_gamma_error_max], fmt='o', color='purple')
+plt.xlabel('Group Counts')
+plt.ylabel('p1.gamma')
+plt.title('p1.gamma vs Group Counts')
+plt.savefig('p1.gamma_vs_Group_Counts.png', dpi=300)
+plt.close()
+plt.figure(figsize=(10, 8))
+plt.errorbar([count for count in range(5, 105, 5)], data_ampl, yerr=[data_ampl_error_min, data_ampl_error_max], fmt='o', color='purple')
+plt.xlabel('Group Counts')
+plt.ylabel('p1.ampl')
+plt.title('p1.ampl vs Group Counts')
+plt.savefig('p1.ampl_vs_Group_Counts.png', dpi=300)
+plt.close()
+plt.figure(figsize=(10, 8))
+plt.errorbar([count for count in range(5, 105, 5)], rstat_list, fmt='o', color='purple')
+plt.xlabel('Group Counts')
+plt.ylabel('Reduced Statistic')
+plt.title('Reduced Statistic vs Group Counts')
+plt.savefig('Reduced_Statistic_vs_Group_Counts.png', dpi=300)
+plt.close()
+plt.figure(figsize=(10, 8))
+plt.errorbar([count for count in range(5, 105, 5)], data_nh, yerr=[data_nh_error_min, data_nh_error_max], fmt='o', color='purple', label='abs1.nH')
+plt.errorbar([count for count in range(5, 105, 5)], data_gamma, yerr=[data_gamma_error_min, data_gamma_error_max], fmt='o', color='green', label='p1.gamma')
+plt.errorbar([count for count in range(5, 105, 5)], data_ampl, yerr=[data_ampl_error_min, data_ampl_error_max], fmt='o', color='red', label='p1.ampl')
+plt.xlabel('Group Counts')
+plt.ylabel('Parameters')
+plt.title('Parameters vs Group Counts')
+plt.legend()
+plt.savefig('Parameters_vs_Group_Counts.png', dpi=300)
+"
+python -c "$python_code_groupcounts"
 
-# # Remove the leading comma
-# radius_values_str=${radius_values_str:1}
-# photon_values_str=${photon_values_str:1}
-# rate_values_str=${rate_values_str:1}
-
-# # 保存photon_count随radius变化的图像
-# python_code_photoncount="
-# import matplotlib.pyplot as plt
-# radius_values = [float(x) for x in '${radius_values_str}'.split(',')]
-# photon_values = [float(x) for x in '${photon_values_str}'.split(',')]
-# plt.plot(radius_values, photon_values)
-# plt.xlabel('Radius')
-# plt.ylabel('Photon')
-# plt.xlim(0,30)
-# # plt.yscale('log')
-# plt.title('Photon vs Radius')
-# plt.savefig('photon_vs_radius.png')
-# "
-# python -c "$python_code_photoncount"
-
-# # 保存photon_rate随radius变化的图像
-# python_code_photonrate="
-# import matplotlib.pyplot as plt
-# radius_values = [float(x) for x in '${radius_values_str}'.split(',')]
-# rate_values = [float(x) for x in '${rate_values_str}'.split(',')]
-# plt.plot(radius_values, rate_values)
-# plt.xlabel('Radius')
-# plt.ylabel('Photon_rate')
-# plt.xlim(0,30)
-# # plt.yscale('log')
-# plt.title('Photon_rate vs Radius')
-# plt.savefig('photon_rate_vs_radius.png')
-# "
-# python -c "$python_code_photonrate"
-
-# echo "radius_final: ${radius_final}"
-# specextract "${pfolder}/${obsid}/repro/acisf0${obsid1}_repro_evt2.fits[sky=circle(${ra},${dec},${radius_final})]" ${name} verbose=0 clob+
-# # read -p 'enter the energy lower limit (keV), e.g., 0.4 ' elow
-# # read -p 'enter the energy upper limit (keV), e.g., 6.0 ' ehigh
-# elow=0.4
-# ehigh=6.0
-# #只对reduced statistic进行判断
-# python_code_groupcount="
-# from sherpa.astro.ui import *
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# for count in range(5, 35, 5):
-#     clean()
-#     load_data(\"${name}.pi\")
-#     group_counts(count)
-#     notice(${elow},${ehigh})
-#     set_source( xsphabs.abs1 * powlaw1d.p1)
-#     abs1.nH = 0.5
-#     guess(p1)
-#     fit()
-
-#     if count==10:
-#         # 寻找单色能量(a monochromatic energy)
-#         flux_data = get_model_plot()
-#         # xhi and xlo is the range of energy
-#         egrid = (flux_data.xhi+flux_data.xlo)/2.0
-#         # y is the predicted flux by the model
-#         flux = flux_data.y
-#         mean_e = np.sum(flux*egrid)/np.sum(flux)
-#         np.savetxt(\"mean_e.txt\", [mean_e], fmt=\"%.2f\")
-
-#     set_conf_opt(\"sigma\", 1.6)
-#     conf()
-#     conf_results=get_conf_results()
-#     best_fit_values=conf_results.parvals
-#     errors=(conf_results.parmins, conf_results.parmaxes)
-#     errors_pecentage=[]
-#     for i in range(3):
-#         error_1=errors[0][i]/best_fit_values[i]
-#         error_2=errors[1][i]/best_fit_values[i]
-#         errors_pecentage.append(max(error_1, error_2))
-#         #设置到小数点后两位
-#         errors_pecentage[i] = round(errors_pecentage[i], 2)
-#     # 对ID为1的数据集, 设置x轴的单位为能量, y轴的单位为计数率, 并且不对数据进行任何缩放。
-#     set_analysis(1, \"energy\", \"rate\", factor=1)
-#     # 绘制模型的预测光谱，而不是原始的观测数据。
-#     # 这个函数会根据当前设置的模型参数，计算模型在每个能量点的预测值，并将这个预测光谱绘制出来。
-#     plot_fit_delchi()
-#     plt.savefig(\"source_flux_chart_\"+str(count)+\".png\")
-#     # 创建一个表格的数据
-#     table_data = [[\"count\", count, \"\"],
-#                 [\"\", \"Best-fit\", \"Error\"],
-#                 [\"abs1.nH\", best_fit_values[0], errors_pecentage[0]],
-#                 [\"p1.gamma\", best_fit_values[1], errors_pecentage[1]],
-#                 [\"p1.ampl\", best_fit_values[2], errors_pecentage[2]]]
-#     # 打开一个txt文件，如果这个文件不存在，那么就创建这个文件
-#     with open(\"table.txt\", \"a\") as file:
-#         # 遍历表格的每一行
-#         for row in table_data:
-#             # 将这一行的数据转换为字符串，并用制表符连接这些字符串
-#             line = \"\t\".join(str(x) for x in row)
-#             # 将这一行的数据写入文件，并在最后添加一个换行符
-#             file.write(line + \"\n\")
-#     from sherpa_contrib.chart import *
-#     save_chart_spectrum(\"source_flux_chart_\"+str(count)+\".dat\", elow=${elow}, ehigh=${ehigh})
-# "
-# python -c "$python_code_groupcount"
+# if count==10:
+    #     # 寻找单色能量(a monochromatic energy)
+    #     flux_data = get_model_plot()
+    #     # xhi and xlo is the range of energy
+    #     egrid = (flux_data.xhi+flux_data.xlo)/2.0
+    #     # y is the predicted flux by the model
+    #     flux = flux_data.y
+    #     mean_e = np.sum(flux*egrid)/np.sum(flux)
+    #     np.savetxt(\"mean_e.txt\", [mean_e], fmt=\"%.2f\")
 
 # mean_e=$(cat mean_e.txt)
 # rm mean_e.txt
